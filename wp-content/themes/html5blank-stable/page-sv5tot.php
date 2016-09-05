eeef6340518f07b221ae9862003d86ebfddb646d
Hoang Doan <hoang.doan@outlook.com>
2016-09-05 21:21:28 +0700

    Fix "love" button position

--- wp-content/themes/html5blank-stable/page-sv5tot.php
+++ wp-content/themes/html5blank-stable/page-sv5tot.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="container" style="margin-top: 70px">
+<div class="container top-list-container" style="margin-top: 70px">
 	<div class="page-heading">
 		<h2>TOP RATED</h2>
 	</div>
@@ -72,6 +72,213 @@
 			</div>
 		</li>
 	</ul>
+	<div class="student-list-dots"></div>
 </div>
+
+<div class="container random-list-container">
+	<div class="page-heading">
+		<h2>RANDOM LIST</h2>
+	</div>
+	<ul class="row student-list">
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+	</ul>	
+</div>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('.student-list').slick({
+		  responsive: [
+		    {
+		    	breakpoint: 768,
+		    	settings: {
+		    		slidesToShow: 1,
+		    		slidesToScroll: 1,
+					dots: true,
+					arrows: false,
+					infinite: true,
+					speed: 300,
+					appendDots: ".student-list-dots"
+		      	}
+		    }
+
+		  ]
+		});
+	})
+</script>
 
 <?php get_footer(); ?>
