851ff57dc6bcd01343ce3b81bc54f00ef6d80340
Hoang Doan <hoang.doan@outlook.com>
2016-09-05 16:59:36 +0700

    Created Student Box

---
+++ wp-content/themes/html5blank-stable/page-sv5tot.php
@@ -0,0 +1,77 @@
+<?php get_header(); ?>
+
+<div class="container" style="margin-top: 70px">
+	<div class="page-heading">
+		<h2>TOP RATED</h2>
+	</div>
+	<ul class="row student-list">
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+		<li class="col-md-4 col-sm-4 student-item">
+			<div class="student-item-content clearfix">
+				<div class="img-container">
+					<a href="#">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
+					</a>
+				</div>
+				<div class="col-xs-12 student-info-container">
+					<div class="student-info clearfix">
+						<div class="col-xs-8">
+							<a href="#"><h4>Lorem opsum</h4></a>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+						</div>
+						<div class="col-xs-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</li>
+	</ul>
+</div>
+
+<?php get_footer(); ?>
