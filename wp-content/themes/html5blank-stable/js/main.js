275055dec663adf541349f2b9f856e6c7e1f6f17
Hoang Doan <hoang.doan@outlook.com>
2016-09-06 14:39:19 +0700

    Add Close Button in Modal (mobile version)

--- wp-content/themes/html5blank-stable/js/main.js
+++ wp-content/themes/html5blank-stable/js/main.js
@@ -21,15 +21,24 @@ $(document).ready(function() {
 	  ]
 	});
 
-	var $grid = $('#random-list').masonry({
+	// var $grid = $('#random-list').masonry({
+	// 	itemSelector: '.student-item',
+	// 	columnWidth: '.student-item',
+	// 	percentPosition: true
+	// });
+
+	// $grid.imagesLoaded().progress( function() {
+ //  		$grid.masonry('layout');
+ //  		console.log("aaa")
+	// });
+
+	var $grid = $('#random-list').imagesLoaded( function() {
+	  // init Masonry after all images have loaded
+	  $grid.masonry({
 		itemSelector: '.student-item',
 		columnWidth: '.student-item',
 		percentPosition: true
-	});
-
-	$grid.imagesLoaded().progress( function() {
-  		$grid.masonry('layout');
-  		console.log("aaa")
+	  });
 	});
 
 })
