45b5fde7e390da2195ff2d14d1a24e79a38ef8cd
DESKTOP-9AHV2P6\AnhPhuong <phuongit247@gmail.com>
2016-09-06 15:58:25 +0700

    fix css page Home

--- wp-content/themes/html5blank-stable/page-home.php
+++ wp-content/themes/html5blank-stable/page-home.php
@@ -3,12 +3,10 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-6 home_page_left">
-          <div class="text-center">
-            <div class="home_page_left_detail">
-              <p>TUẦN NÀY</p>
-              <p>TÀI CHÍNH CÓ GÌ?</p>
-              <a >XEM THÊM</a>
-            </div>
+          <div class="text-center home_page_left_detail">
+            <p>TUẦN NÀY</p>
+            <p>TÀI CHÍNH CÓ GÌ?</p>
+            <a >XEM THÊM</a>
           </div>
           <div class="logo_rawa">
             <img src="<?php echo get_template_directory_uri();?>/img/LogoRawa.png">
