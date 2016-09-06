6f8d17004216abb22abf04b7b644c78cf9552f08
DESKTOP-9AHV2P6\AnhPhuong <phuongit247@gmail.com>
2016-09-06 15:49:49 +0700

    fix name class

--- wp-content/themes/html5blank-stable/page-home.php
+++ wp-content/themes/html5blank-stable/page-home.php
@@ -2,9 +2,9 @@
 
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm-6 Home_page_left">
-          <div class="text-center Home_page_left_border">
-            <div class="Home_page_left_detail">
+        <div class="col-sm-6 home_page_left">
+          <div class="text-center">
+            <div class="home_page_left_detail">
               <p>TUẦN NÀY</p>
               <p>TÀI CHÍNH CÓ GÌ?</p>
               <a >XEM THÊM</a>
@@ -14,8 +14,8 @@
             <img src="<?php echo get_template_directory_uri();?>./img/LogoRawa.png">
           </div>
         </div>
-        <div class="col-sm-6 Home_page_Right">
-          <div class="text-center Home_page_Right_detail">
+        <div class="col-sm-6 home_page_Right">
+          <div class="text-center home_page_Right_detail">
             <p>SINH VIÊN NĂM TỐT</p>
             <a href="<?php echo get_site_url(); ?>/sv5tot/">XEM THÊM</a>
           </div>
