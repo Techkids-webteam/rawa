b09c6153703678a4835301ac1075ffa893fdac0d
Hoang Doan <hoang.doan@outlook.com>
2016-09-06 11:43:16 +0700

    Modal Student Detail (at first image)

--- wp-content/themes/html5blank-stable/page-sv5tot.php
+++ wp-content/themes/html5blank-stable/page-sv5tot.php
@@ -1,5 +1,36 @@
 <?php get_header(); ?>
 
+<!--MODAL -->
+<div class="modal fade bs-example-modal-lg" id="modal-student" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content modal-student">
+      <div class="row">
+      	<div class="col-sm-6 modal-student-img">
+      		<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student_3.jpg">
+      	</div>
+      	<div class="col-sm-6 modal-student-description">
+			<div class="student-info clearfix">
+				<div class="col-xs-8">
+					<a href="#"><h4>Lorem opsum</h4></a>
+				</div>
+				<div class="col-xs-4">
+					<button class="btn btn-default btn-rated"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span> 1000</button>
+				</div>
+				<div class="col-xs-12">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor </p>
+				</div>
+			</div>
+      	</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <div class="container top-list-container">
 	<div class="page-heading">
 		<h2>TOP RATED</h2>
@@ -8,7 +39,7 @@
 		<li class="col-md-4 col-sm-4 student-item">
 			<div class="student-item-content clearfix">
 				<div class="img-container">
-					<a href="#">
+					<a href="#" data-toggle="modal" data-target="#modal-student">
 						<img src="<?php echo get_template_directory_uri(); ?>/img/sample_student.jpg">
 					</a>
 				</div>
