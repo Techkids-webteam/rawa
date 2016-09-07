2f6172459444ab119cea88a7b2e248d9506cb6fa
AVAVT <itsavavt@gmail.com>
2016-09-07 13:59:38 +0700

    fix homepage css

--- wp-content/themes/html5blank-stable/page-home.php
+++ wp-content/themes/html5blank-stable/page-home.php
@@ -3,16 +3,18 @@
     <div class="row">
       <div class="col-sm-6 home_page_left">
         <div class="text-center home_page_left_detail">
-          <p>TUẦN NÀY</p>
-          <p>TÀI CHÍNH CÓ GÌ?</p>
-          <a >XEM THÊM</a>
+          <div class="home_page_left_content">
+            <p>TUẦN NÀY</p>
+            <p>TÀI CHÍNH CÓ GÌ?</p>
+            <a href="#">XEM THÊM</a>
+          </div>
         </div>
         <div class="logo_rawa">
           <img src="<?php echo get_template_directory_uri();?>/img/LogoRawa.png">
         </div>
       </div>
-      <div class="col-sm-6 home_page_Right">
-        <div class="text-center home_page_Right_detail">
+      <div class="col-sm-6 home_page_right">
+        <div class="text-center home_page_right_detail">
           <p>SINH VIÊN NĂM TỐT</p>
           <a href="<?php echo get_site_url(); ?>/sv5tot/">XEM THÊM</a>
         </div>
